6ae0cc4afb0bc6bacdef69815e46176612cc55d1
Yerkebulan-bit <63576399+Yerkebulan-bit@users.noreply.github.com>
2022-05-08 16:10:10 +0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -8,3 +8,7 @@ CREATE TABLE student
     student_name VARCHAR(20) NOT NULL,
     student_grade VARCHAR(20) NOT NULL
 );
+
+INSERT INTO student (student_name, student_grade) VALUES('Yerkebulan', '5th grade');
+INSERT INTO student (student_name, student_grade) VALUES('Roman', '11th grade');
+INSERT INTO student (student_name, student_grade) VALUES('Timur', '1st grade');
